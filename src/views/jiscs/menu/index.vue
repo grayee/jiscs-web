45cbfd3f0f29b685967dc590557e946634ed44c5
Gray.Z <zrg523@gmail.com>
2020-03-16 21:17:47 +0800

    init update

--- src/views/jiscs/menu/index.vue
+++ src/views/jiscs/menu/index.vue
@@ -1,106 +1,111 @@
 <!-- 1.模板 :html 结构-->
 <template>
-  <!-- Main content -->
   <div class="app-container">
-    <vxe-table
-      :data="tableData"
-      :tree-config="{children: 'children'}">
-      <vxe-table-column type="seq"></vxe-table-column>
-      <vxe-table-column field="name" title="Name" tree-node></vxe-table-column>
-      <vxe-table-column field="sex" title="Sex"></vxe-table-column>
-      <vxe-table-column field="date" title="Date"></vxe-table-column>
-    </vxe-table>
-
-
-<!--    <vxe-grid
+    <vxe-grid
+      ref="xGrid"
       border
       resizable
       tree-config
+      :form-config="tableForm"
+      :toolbar="tableToolbar"
       :proxy-config="tableProxy"
-      :columns="tableColumn"></vxe-grid>-->
+      :columns="tableColumn"
+      @toolbar-button-click="toolbarButtonClickEvent"
+    >>
+    </vxe-grid>
 
-    <!-- /.content -->
+    <vxe-modal ref="xModal" v-model="showDialog" :title="dialogTitle" width="800" resize destroy-on-close>
+      <vxe-form
+        :data="formData"
+        :rules="formRules"
+        title-align="right"
+        title-width="100"
+        size="mini"
+        @submit="submitEvent"
+      >
+        <vxe-form-item
+          title="上级菜单"
+          field="name"
+          span="12"
+          :item-render="{name: 'input', attrs: {placeholder: '请输入名称'}}"
+        />
+        <vxe-form-item title="菜单类型" field="nickname" span="12">
+          <vxe-radio-group v-model="formData.nickname">
+            <vxe-radio label="1">目录</vxe-radio>
+            <vxe-radio label="0">菜单</vxe-radio>
+            <vxe-radio label="0">按钮</vxe-radio>
+          </vxe-radio-group>
+        </vxe-form-item>
+        <vxe-form-item
+          title="菜单名称"
+          field="role"
+          span="12"
+          :item-render="{name: 'input', attrs: {placeholder: '请输入角色'}}"
+        />
+        <vxe-form-item title="菜单图标" field="sex" span="12" :item-render="{name: '$select', options: sexList}" />
+        <vxe-form-item
+          title="菜单路径"
+          field="age"
+          span="12"
+          :item-render="{name: 'input', attrs: {type: 'number', placeholder: '请输入年龄'}}"
+        />
+        <vxe-form-item
+          title="组件路径"
+          field="num"
+          span="12"
+          :item-render="{name: 'input', attrs: {type: 'number', placeholder: '请输入数值'}}"
+        />
+        <vxe-form-item
+          title="排序编码"
+          field="date3"
+          span="12"
+          :item-render="{name: 'input', attrs: {type: 'date', placeholder: '请选择日期'}}"
+        />
+        <vxe-form-item
+          title="权限编码"
+          field="address"
+          span="12"
+          :title-suffix="{message: '啦啦啦，就是这么强大！！！', icon: 'fa fa-question-circle'}"
+          :item-render="{name: 'textarea', attrs: {placeholder: '请输入地址'}}"
+        />
+        <vxe-form-item
+          title="备注信息"
+          field="date3"
+          span="12"
+          :item-render="{name: 'input', attrs: {type: 'date', placeholder: '请选择日期'}}"
+        />
+        <vxe-form-item align="center" span="24">
+          <vxe-button type="submit" status="primary">保存</vxe-button>
+          <vxe-button type="reset">重置</vxe-button>
+          <vxe-button @click="$refs.xModal.close()">取消</vxe-button>
+        </vxe-form-item>
+      </vxe-form>
+    </vxe-modal>
   </div>
 </template>
-
 <!-- 2.行为 :处理逻辑-->
 <script>
+import XEUtils from 'xe-utils'
+
 export default {
-  name: 'Menu',
   data() {
     return {
-      msg: null,
       loading: false,
-      tableData:[{
-        name: 'test1',
-        sex: '男',
-        date: '2019-08',
-        children: [
-          {
-            name: 'test2',
-            sex: '女',
-            date: '2019-08',
-            children: [
-              {
-                name: 'test3',
-                sex: '男',
-                date: '2019-08',
-              },
-              {
-                name: 'test11',
-                sex: '男',
-                date: '2019-08',
-              }
-            ]
-          },
-          {
-            name: 'test7',
-            sex: '女',
-            date: '2019-08',
-            children: [
-              {
-                name: 'test9',
-                sex: '男',
-                date: '2019-08',
-              }
-            ]
-          }
-        ]
-      },
-        {
-          name: 'test4',
-          sex: '男',
-          date: '2019-08',
-          children: [
-            {
-              name: 'test5',
-              sex: '女',
-              date: '2019-08',
-            },
-            {
-              name: 'test15',
-              sex: '女',
-              date: '2019-08',
-            }
-          ]
-        }],
-      tableForm: {
-        titleWidth: 100,
-        titleAlign: 'right',
-        items: [
-          { field: 'companyName', title: '公司名称', span: 6, titlePrefix: { message: 'app.body.valid.rName', icon: 'fa fa-exclamation-circle' }, itemRender: { name: '$input', props: { placeholder: '请输入名称' }}},
-          { field: 'role', title: '角色', span: 6, itemRender: { name: '$input', props: { placeholder: '请输入角色' }}},
-          { field: 'nickname', title: '昵称', span: 6, itemRender: { name: '$input', props: { placeholder: '请输入昵称' }}},
-          { field: 'sex', title: '性别', span: 6, folding: true, titleSuffix: { message: '注意，必填信息！', icon: 'fa fa-info-circle' }, itemRender: { name: '$select', options: [] }},
-          { field: 'age', title: '年龄', span: 6, folding: true, itemRender: { name: '$input', props: { type: 'number', placeholder: '请输入年龄' }}},
-          { span: 24, align: 'right', collapseNode: true, itemRender: { name: '$buttons', children: [{ props: { type: 'submit', content: 'app.body.label.search', status: 'primary' }}, { props: { type: 'reset', content: 'app.body.label.reset' }}] }}
-        ]
+      tableProxy: {
+        form: true, // 启用表单代理
+        ajax: {
+          // 处理树结构转换
+          query: () => this.findList(),
+          delete: ({ body }) => this.findList(),
+          save: ({ body }) => this.findList()
+        }
       },
       tableToolbar: {
         buttons: [
-          { code: 'insert_actived', name: '新增', icon: 'fa fa-plus' },
-          { code: 'mark_cancel', name: '删除/取消', icon: 'fa fa-trash-o' },
-          { code: 'save', name: 'app.body.button.save', icon: 'fa fa-save' }
+          { code: 'add', name: '新增', icon: 'fa fa-plus' },
+          { code: 'edit', name: '修改', icon: 'fa fa-edit' },
+          { code: 'mark_cancel', name: '删除', icon: 'fa fa-trash-o' },
+          { code: 'save', name: '保存', icon: 'fa fa-save' }
         ],
         refresh: true,
         export: true,
@@ -109,107 +114,118 @@ export default {
         custom: true
       },
       tableColumn: [
-        { field: 'name', title: '名称' },
-        { field: 'sex', title: '大小' },
-        { field: 'date', title: '创建时间'}
+        { type: 'checkbox', width: 50 }, { type: 'seq', width: 60 },
+        { field: 'name', title: '菜单名称', treeNode: true },
+        { field: 'url', title: '菜单路径' },
+        { field: 'component', title: '组件' },
+        { field: 'type', title: '菜单类型' },
+        { field: 'orderCode', title: '排序码' },
+        { field: 'authCode', title: '权限编码' },
+        { field: 'modifyDate', title: '更新时间', formatter: this.formatterDate }
       ],
-      tableProxy: {
-        form: true, // 启用表单代理
-        ajax: {
-          // page 对象： { pageSize, currentPage }
-          query: ({ page, sort, filters, form }) => this.findList({ page, sort, filters, form })
-
-        }
+      tableForm: {
+        titleWidth: 100,
+        titleAlign: 'right',
+        items: [
+          {
+            field: 'name',
+            title: '菜单名称',
+            span: 6,
+            titlePrefix: { message: 'app.body.valid.rName', icon: 'fa fa-exclamation-circle' },
+            itemRender: { name: '$input', props: { placeholder: '请输入名称' }}
+          },
+          {
+            span: 10,
+            align: 'right',
+            collapseNode: false,
+            itemRender: {
+              name: '$buttons',
+              children: [
+                { props: { type: 'submit', content: 'app.body.label.search', status: 'primary' }},
+                { props: { type: 'reset', content: 'app.body.label.reset' }}]
+            }
+          }
+        ]
+      },
+      showDialog: false,
+      dialogTitle: '',
+      formData: {
+        name: '',
+        nickname: '',
+        sex: '',
+        age: 30,
+        status: '1',
+        region: null,
+        weight: null,
+        date: null,
+        single: '1'
+      },
+      formRules: {
+        name: [
+          { required: true, message: '请输入名称' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符' }
+        ],
+        nickname: [
+          { required: true, message: '请输入昵称' }
+        ],
+        sex: [
+          { required: true, message: '请选择性别' }
+        ]
       }
     }
   },
   mounted() {
-    this.msg = '123'
-  },
-  created() {
-
   },
   methods: {
-    guide() {
-      console.log('1111')
-    },
     findList(param) {
       this.loading = true
       const filters = []
-      return this.$api.menu.getMenuTree({
+      return this.$api.menu.getMenuList({
         pageNo: 1,
-        pageSize: 20,
+        pageSize: 1000,
         queryFilters: filters
       }).then((response) => {
         this.loading = false
-        return [{
-          name: 'test1',
-          sex: '男',
-          date: '2019-08',
-          children: [
-            {
-              name: 'test2',
-              sex: '女',
-              date: '2019-08',
-              children: [
-                {
-                  name: 'test3',
-                  sex: '男',
-                  date: '2019-08',
-                },
-                {
-                  name: 'test11',
-                  sex: '男',
-                  date: '2019-08',
-                }
-              ]
-            },
-            {
-              name: 'test7',
-              sex: '女',
-              date: '2019-08',
-              children: [
-                {
-                  name: 'test9',
-                  sex: '男',
-                  date: '2019-08',
-                }
-              ]
-            }
-          ]
-        },
-          {
-            name: 'test4',
-            sex: '男',
-            date: '2019-08',
-            children: [
-              {
-                name: 'test5',
-                sex: '女',
-                date: '2019-08',
-              },
-              {
-                name: 'test15',
-                sex: '女',
-                date: '2019-08',
-              }
-            ]
-          }]
+        this.tableData = XEUtils.toArrayTree(response.data.content,
+          { key: 'id', parentKey: 'parentId', children: 'children' })
+        return this.tableData
       }).catch(error => {
         console.log('error', error)
       })
     },
-    handlePageChange({ currentPage, pageSize }) {
-      this.tablePage.currentPage = currentPage
-      this.tablePage.pageSize = pageSize
-      this.findList()
+    toolbarButtonClickEvent({ code }, event) {
+      switch (code) {
+        case 'add':
+          // this.$XModal.alert(code)
+          this.showDialog = true
+          this.dialogTitle = '新增'
+          break
+        case 'edit':
+          const xGrid = this.$refs.xGrid
+          const removeRecords = xGrid.getCheckboxRecords()
+          if (removeRecords.length > 0) {
+            this.showDialog = true
+            this.dialogTitle = '编辑'
+          } else {
+            this.$XModal.message({ message: '请至少选择一条记录', status: 'warning' })
+          }
+          break
+      }
+    },
+    submitEvent() {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        this.$XModal.message({ message: '保存成功', status: 'success' })
+      }, 1000)
+    },
+    formatterDate({ cellValue }) {
+      return XEUtils.toDateString(cellValue, 'yyyy-MM-dd HH:mm:ss')
     }
   }
 }
 </script>
-
 <!-- 3.样式:解决样式     -->
 <style scoped>
 
 </style>
-
